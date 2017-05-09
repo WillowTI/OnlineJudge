45051b1b21d503c5f151f227f4b204828e45481a
riversideswamp <liu980726@gmail.com>
2017-05-09 23:22:39 +0800

    Create UVa11988.c

---
+++ Vjudge/UVa11988.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+
+const int maxn=100000+5;
+int last;
+int cur;
+int next[100005]={0};
+char s[100005]={0};
+
+
+
+int main()
+{
+    while (scanf("%s",s+1)==1)
+    {
+        int n=strlen(s+1);
+        last=cur=0;
+        next[0]=0;
+        for (int i=1;i<=n;i++)
+        {
+            char ch=s[i];
+            if (ch=='[')
+            {
+                cur=0;
+            }
+            else if (ch==']')
+            {
+                cur=last;
+            }
+            else
+            {
+                next[i]=next[cur];
+                next[cur]=i;
+                if (cur==last)
+                {
+                    last=i;
+                }
+                cur=i;
+            }
+        }
+        for (int i=next[0];i!=0;i=next[i])
+        {
+            printf("%c",s[i]);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
