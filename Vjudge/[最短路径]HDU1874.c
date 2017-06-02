55250516aed08b15ee2cdb029730c230d411b4a8
riversideswamp <liu980726@gmail.com>
2017-06-02 11:38:47 +0800

    Create [最短路径]HDU1874.c

---
+++ Vjudge/[最短路径]HDU1874.c
@@ -0,0 +1,116 @@
+Problem Description
+某省自从实行了很多年的畅通工程计划后，终于修建了很多路。不过路多了也不好，每次要从一个城镇到另一个城镇时，都有许多种道路方案可以选择，而某些方案要比另一些方案行走的距离要短很多。这让行人很困扰。
+
+现在，已知起点和终点，请你计算出要从起点到终点，最短需要行走多少距离。
+
+
+Input
+本题目包含多组数据，请处理到文件结束。
+每组数据第一行包含两个正整数N和M(0<N<200,0<M<1000)，分别代表现有城镇的数目和已修建的道路的数目。城镇分别以0～N-1编号。
+接下来是M行道路信息。每一行有三个整数A,B,X(0<=A,B<N,A!=B,0<X<10000),表示城镇A和城镇B之间有一条长度为X的双向道路。
+再接下一行有两个整数S,T(0<=S,T<N)，分别代表起点和终点。
+
+
+Output
+对于每组数据，请在一行里输出最短需要行走的距离。如果不存在从S到T的路线，就输出-1.
+
+
+Sample Input
+3 3
+0 1 1
+0 2 3
+1 2 1
+0 2
+3 1
+0 1 1
+1 2
+
+
+Sample Output
+2
+-1
+
+分析：
+可能会输入重复的路径，要更新为最小的。
+建立二维数组，这是一个关于主对角线对称的矩阵，主对角线上的元素都是0。
+如果两个点之间没有路径，设置为-1。
+
+Floyd:
+三重循环。
+主循环：从0到n一一设置中转站。(k)
+内部二重循环：以i<->j为例，比较i<->k+j<->k的距离和i<->j。
+当中转走法合法而直接走不合法或者中转走法更近时，更新。
+
+最后，当要求的两点之间的距离大于0小于(n-1)*10000时，输出P(i,j)。
+否则，说明不能走，输出-1。
+
+Floyd算法的时间复杂度很大，这道题200*200*200还行，可以暴力做。
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int n,m,town[205][205]={{0}};
+
+void Floyd();
+
+
+int main()
+{
+    while (scanf("%d%d",&n,&m)!=EOF)
+    {
+
+        memset(town,0,sizeof(town));
+
+        for (int i=0;i<m;i++)
+        {
+            int x,y,dis;
+            scanf("%d%d%d",&x,&y,&dis);
+            if (town[x][y]==0) town[y][x]=town[x][y]=dis;//坑点
+            else town[y][x]=town[x][y]=town[x][y]<dis?town[x][y]:dis;
+        }
+
+        for (int i=0;i<n;i++)
+        {
+            for (int j=0;j<n;j++)
+            {
+                if (i!=j&&town[i][j]==0) town[i][j]=-1;
+            }
+        }
+
+        Floyd();
+
+        int st,end;
+
+        scanf("%d%d",&st,&end);
+
+        if (town[st][end]>=0&&town[st][end]<(n-1)*10000) printf("%d\n",town[st][end]);
+        else printf("-1\n");
+    }
+
+
+
+
+    return 0;
+}
+
+void Floyd()
+{
+    for (int k=0;k<n;k++)
+    {
+        for (int i=0;i<n;i++)
+        {
+            for (int j=0;j<n;j++)
+            {
+                if (town[i][k]!=-1&&town[j][k]!=-1)
+                {
+                    if (town[i][j]==-1||(town[i][j]!=-1&&town[i][k]+town[j][k]<town[i][j]))
+                        town[j][i]=town[i][j]=town[i][k]+town[j][k];
+                }
+            }
+        }
+    }
+}
