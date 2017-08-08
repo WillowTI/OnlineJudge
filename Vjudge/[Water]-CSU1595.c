623d0eec973e6c05ad92c4225d764a44dcc6ec32
riversideswamp <liu980726@gmail.com>
2017-08-08 19:03:13 +0800

    Update [Water]-CSU1595.c

--- Vjudge/[Water]-CSU1595.c
+++ Vjudge/[Water]-CSU1595.c
@@ -1,8 +1,11 @@
+非常巧妙的做法
+把一个时间段涉及到的时间在数组里全部加一
+最后答案就是这个数组里最大的元素
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-
 
 int main()
 {
@@ -12,24 +15,23 @@ int main()
     for (int t = 0 ; t < cas ; t++)
     {
         int n;
+        int time[25]={0};
         scanf("%d" , &n);
-        printf("%d=" , n);
-        int ans[10000]={0};
-        int pos = 0;
-        int i = 2;
-        while (i * i <= n)
+        for (int i = 0 ; i < n ; i++)
         {
-            if (n % i ==0)
+            int st , ed;
+            scanf("%d%d" , &st , &ed);
+            for (int j = st ; j <= ed ; j++)
             {
-                ans[pos++] = i;
-                n /= i;
-                i = 1;
+                time[j]++;
             }
-            i++;
         }
-        ans[pos++] = n;
-        for (int j = 0 ; j < pos ; j++)
-            printf("%d%c" , ans[j] , j == pos - 1 ? '\n' : '*');
+        int ans = -1;
+        for (int i = 0 ; i < 25 ; i++)
+        {
+            ans = ans > time[i] ? ans : time[i];
+        }
+        printf("%d\n" , ans);
     }
     return 0;
 }
