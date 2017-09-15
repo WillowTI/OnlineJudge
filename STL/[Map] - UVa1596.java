6e25492c2646d44d8ce78d22228c2eac59d18afd
riversideswamp <liu980726@gmail.com>
2017-09-15 22:57:00 +0800

    1

--- STL/[Map] - UVa1596.java
+++ STL/[Map] - UVa1596.java
@@ -1,3 +1,21 @@
+//题意：
+//输入一些数组的声明和赋值操作
+//让你判断是否都是合法的
+//不合法的使出哪一行不合法（从1开始计数）
+//不合法的情况只有数组下标越界和使用未赋值的变量
+//合法的输出0
+//分析：
+//声明语句不包含"="
+//赋值有"="
+//声明语句把数组名和大小记录下来
+//可以用HashMap(name-size)
+//赋值语句分别检测"="左右两边是否都合法
+//由于下标也可以用数组元素来表示（如a[b[0]]）
+//所以可以用递归的方法计算其值
+//用HashMap保存哪个数组的下标记录的什么值
+//也就是通过name查找另一个HashMap
+//这个内层的HashMap(index-value)
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -8,24 +26,23 @@ public class Main {
 	
 	static HashMap<String, Integer> name_size = new HashMap<>();
 	static HashMap<String, HashMap<Integer, Integer>> index_value = new HashMap<>();
-	static boolean flag = true;
+	static boolean flag = true;//flag记录这句赋值语句是否合法
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		File f = new File("in.txt");
-		Scanner in = new Scanner(f);
+		//File f = new File("in.txt");
+		Scanner in = new Scanner(System.in);
 		String s;
 		INPUT:
 		while (!(s = in.nextLine()).equals("."))
 		{
 			name_size.clear();
 			index_value.clear();
-			ArrayList<String> order = new ArrayList<>();
+			ArrayList<String> order = new ArrayList<>();//先把所有的命令都保存下来，逐行模拟
 			do 
 			{
 				order.add(s);
 			}while (!(s = in.nextLine()).equals("."));
-			MAIN:
 			for (int i = 0; i < order.size(); i++)
 			{
 				String ord = order.get(i);
@@ -38,21 +55,7 @@ public class Main {
 					int leftindex = calculate(sub[0].substring(sub[0].indexOf('[') + 1, sub[0].length() - 1));
 					if (flag && leftindex >= 0 && leftindex < name_size.get(leftname))
 					{
-						if (sub[1].contains("["))
-						{
-							String rightname = sub[1].substring(0, sub[1].indexOf('['));
-							int rightindex = calculate(sub[1].substring(sub[1].indexOf('[') + 1, sub[1].length() - 1));
-							if (flag && index_value.get(rightname).containsKey(rightindex))
-							{
-								rightvalue = index_value.get(rightname).get(rightindex);
-							}
-							else
-								flag = false;
-						}
-						else
-						{
-							rightvalue = Integer.parseInt(sub[1]);
-						}
+						rightvalue = calculate(sub[1]);
 					}
 					else 
 						flag = false;
@@ -62,9 +65,7 @@ public class Main {
 						continue INPUT;
 					}
 					else 
-					{
 						index_value.get(leftname).put(leftindex, rightvalue);
-					}
 				}
 				else 
 				{
@@ -81,12 +82,14 @@ public class Main {
 	static int calculate(String s)
 	{
 		if (!s.contains("["))
-		{
 			return Integer.parseInt(s);
-		}
-		while (s.indexOf('[') != s.lastIndexOf('['))
+		String name = s.substring(0, s.indexOf('['));
+		int index = calculate(s.substring(s.indexOf('[') + 1, s.length() - 1));
+		if (!flag || index == -1 || !index_value.get(name).containsKey(index) || index_value.get(name).get(index) == -1)
 		{
-			
+			flag = false;
+			return -1;//一旦检测到不合法的就返回-1，因为题目说了数组的元素的都是非负的，这样便于后续的判断
 		}
+		return index_value.get(name).get(index);
 	}
 }
